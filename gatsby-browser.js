175f28288194f4f137b9c48acec61353d4c89dea
Fred Hawk <fredrichawk@gmail.com>
2020-03-31 08:00:52 +0200

    feat: Added styling to blog posts.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,26 @@
+import { MDXProvider } from "@mdx-js/react"
 import "prismjs/themes/prism.css"
+import React from "react"
+
+const components = {
+  wrapper: ({ children }) => <>{children}</>,
+  h2: props => (
+    <h2 className="mb-8 text-lg text-green-600" {...props}>
+      {props.children}
+    </h2>
+  ),
+  h3: props => (
+    <h3 className="mb-6 text-md" {...props}>
+      {props.children}
+    </h3>
+  ),
+  p: props => (
+    <p className="mb-4 leading-relaxed text-sm" {...props}>
+      {props.children}
+    </p>
+  ),
+}
+
+export const wrapRootElement = ({ element }) => {
+  return <MDXProvider components={components}>{element}</MDXProvider>
+}
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,26 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import { MDXProvider } from "@mdx-js/react"
+import "prismjs/themes/prism.css"
+import React from "react"
 
-// You can delete this file if you're not using it
+const components = {
+  wrapper: ({ children }) => <>{children}</>,
+  h2: props => (
+    <h2 className="mb-8 text-lg text-green-600" {...props}>
+      {props.children}
+    </h2>
+  ),
+  h3: props => (
+    <h3 className="mb-6 text-md" {...props}>
+      {props.children}
+    </h3>
+  ),
+  p: props => (
+    <p className="mb-4 leading-relaxed text-sm" {...props}>
+      {props.children}
+    </p>
+  ),
+}
+
+export const wrapRootElement = ({ element }) => {
+  return <MDXProvider components={components}>{element}</MDXProvider>
+}
