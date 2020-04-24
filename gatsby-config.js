f62bb3c98a0477e3daf16ee372c84ddc06a447e9
Fred Hawk <fredrichawk@gmail.com>
2020-04-24 06:22:49 +0200

    fix: Added categories to rss feed.

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,6 +99,7 @@ module.exports = {
                 return Object.assign({}, edge.node.frontmatter, {
                   description: edge.node.excerpt,
                   date: edge.node.frontmatter.date,
+                  categories: edge.node.frontmatter.tags,
                   url: `${site.siteMetadata.siteUrl}/${edge.node.fields.slug}`,
                   guid: `${site.siteMetadata.siteUrl}/${edge.node.fields.slug}`,
                   custom_elements: [{ "content:encoded": edge.node.html }],
@@ -119,6 +120,7 @@ module.exports = {
                       frontmatter {
                         title
                         date
+                        tags
                       }
                     }
                   }
