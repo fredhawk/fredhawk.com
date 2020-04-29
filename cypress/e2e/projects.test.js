8c4e1607daf30c3e6ee736c0d8561bd71db49db0
Fred Hawk <fredrichawk@gmail.com>
2020-04-29 06:38:05 +0200

    feat: Added tests to check that the projects are listed.

--- cypress/e2e/projects.test.js
+++ cypress/e2e/projects.test.js
@@ -10,4 +10,16 @@ describe("Project Page", () => {
   it("Find four projects listed", () => {
     cy.findAllByTestId("project").should("be", 4)
   })
+  it("Should have a Budget Bears project", () => {
+    cy.findAllByText(/budget bears/i).should("exist")
+  })
+  it("Should have a Bookfinder project", () => {
+    cy.findAllByText(/bookfinder/i).should("exist")
+  })
+  it("Should have a Blue Candlenut project", () => {
+    cy.findAllByText(/blue candlenut/i).should("exist")
+  })
+  it("Should have a Resume Bears project", () => {
+    cy.findAllByText(/resume bears/i).should("exist")
+  })
 })
