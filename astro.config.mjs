b6720c683b57abf9dc4a76675783d539e0aa4a53
Fred Hawk <fredrichawk@gmail.com>
2023-09-05 19:49:25 +0200

    chore: Migrated blog and projects.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,23 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	// experimental: {
+	// 	assets: true,
+	// 	viewTransitions: true
+	// },
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		shikiConfig: {
+			// Choose from Shiki's built-in themes (or add your own)
+			// https://github.com/shikijs/shiki/blob/main/docs/themes.md
+			theme: 'nord',
+			// Add custom languages
+			// Note: Shiki has countless langs built-in, including .astro!
+			// https://github.com/shikijs/shiki/blob/main/docs/languages.md
+			langs: ['js', 'css', 'html', 'ts', 'astro', 'svelte', 'rust', 'bash'],
+			// Enable word wrap to prevent horizontal scrolling
+			wrap: true,
+		},
+	},
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,26 @@
 import { defineCollection, z } from 'astro:content';
 
+const projects = defineCollection({
+	type: 'content',
+	schema: ({ image }) => z.object({
+		title: z.string(),
+		description: z.string(),
+		url: z.string(),
+		githublink: z.string(),
+		heroImage: image(),
+		// heroImage: image().refine((img) => img.width >= 1080, {
+		// 	message: "Cover image must be at least 1080 pixels wide!",
+		// }),
+		heroAlt: z.string().optional(),
+		type: z.string(),
+		tags: z.array(z.string()),
+		published: z.boolean()
+	})
+})
+
 const blog = defineCollection({
 	// Type-check frontmatter using a schema
+	type: 'content',
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
@@ -15,7 +34,10 @@ const blog = defineCollection({
 			.optional()
 			.transform((str) => (str ? new Date(str) : undefined)),
 		heroImage: z.string().optional(),
+		type: z.string(),
+		tags: z.array(z.string()),
+		published: z.boolean()
 	}),
 });
 
-export const collections = { blog };
+export const collections = { blog, projects };
